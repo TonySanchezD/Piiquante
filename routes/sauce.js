20b5563a5f8a70e905c3e1f352ed8e68249c08ed
Tony Sanchez <tony-sanchez@hotmail.fr>
2022-10-21 17:50:20 +0200

    controller user terminé

---
+++ routes/sauce.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const router = express.Router()
+const auth = require('../middleware.auth')
+
+
+router.get('/', )
+
+router.get('/:id', )
+
+router.post('/', )
+
+router.put('/:id', )
+
+router.delete('/:id', )
+
+router.post('/:id/like', )
