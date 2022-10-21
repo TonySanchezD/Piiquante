c8a616f3c047a865ad5a801fa56369cc1099e710
Tony Sanchez <tony-sanchez@hotmail.fr>
2022-10-21 18:44:51 +0200

    Model sauce créé

---
+++ models/sauce.js
@@ -0,0 +1,17 @@
+const mongoose =  require('mongoose')
+
+const sauceShema = mongoose.Schema({
+    userId: { type: String, require: true},
+    name: { type: String, require: true},
+    manufacturer: { type: String, require: true},
+    description: { type: String, require: true},
+    mainPepper: { type: String, require: true},
+    ImageUrl: { type: String, require: true},
+    heat: { type: Number, require: true},
+    likes: { type: Number},
+    dislikes: { type: Number},
+    usersLiked: { type: [String]},
+    usersDisliked: { type: [String]},
+})
+
+module.exports = mongoose.model('Sauce', sauceShema)
